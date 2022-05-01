1ef7bf4a7b62f42401faf45cf07957cfdd9390f6
anujyog1 <66674832+anujyog1@users.noreply.github.com>
2022-05-02 00:48:30 +0530

    uploading my project maven-example-jar to git repository

---
+++ maven-example-jar/src/main/java/com/journal/maven/maven_example_jar/App.java
@@ -0,0 +1,13 @@
+package com.journal.maven.maven_example_jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello ---World! in mavan java" );
+    }
+}
